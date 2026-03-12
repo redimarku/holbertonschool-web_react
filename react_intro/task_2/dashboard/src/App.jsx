0698fd5e5e838b7c3246e85a941a5164ef2a6890
12785@holbertonstudents.com <you@example.com>
2026-03-12 12:58:20 +0100

    task_2

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -13,16 +13,21 @@ const App = () => {
         <img src={image3} alt='holberton logo' style={{ width: '300px', height: '350px' }} />
         <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-      <div className='App-body'>
-        <p>Login to access the full dashboard</p>
-        <label htmlFor='email'>Email</label>
-        <input type='email' id='email' />
-        <br />
-        <label htmlFor='password'>Password</label>
-        <input type='password' id='password' />
-        <br />
-        <button type='submit'>OK</button> 
-      </div>
+     <div className='App-body'>
+      <p>Login to access the full dashboard</p>
+
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" />
+
+      <br />
+
+      <label htmlFor="password">Password</label>
+      <input type="password" id="password" />
+
+      <br />
+
+      <button type="button">OK</button>
+  </div>
       <div className='App-footer'>
         <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
       </div>
