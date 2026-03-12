9707fa61d85554b1b65d31b5b2d22d1caee14907
12785@holbertonstudents.com <you@example.com>
2026-03-12 13:13:11 +0100

    task_2

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <div className='App-body'>
          <p >Login to access the full dashboard</p>
         <label htmlFor='email' >Email</label>
-        <input type='text'  id='email' />
+        <input type='email' id='email' />
         <label htmlFor='password' >Password</label>
         <input type='password'   id='password' />
         <button>OK</button>
