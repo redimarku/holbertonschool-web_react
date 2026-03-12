20ee3efbfd19bfdbde0133acaf874b8480ecceaa
12785@holbertonstudents.com <you@example.com>
2026-03-12 14:35:29 +0100

    Initial commit

--- react_intro/task_3/dashboard/src/Notifications.jsx
+++ react_intro/task_3/dashboard/src/Notifications.jsx
@@ -3,31 +3,33 @@ import closeButton from './assets/close-button.png';
 import { getLatestNotification } from './utils';
 
 const Notifications = () => {
-    const handleClick = () => {
-        console.log("Close button has been clicked");
-    }
-    
-    return (
-        <div className="notification-items">
-            <p>Here is the list of notifications</p>
-            <ul>
-                <li data-priority="default">New course available</li>
-                <li data-priority="urgent">New resume available</li>
-                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
-            </ul>
-            <button 
-                style={{float:'right'}} 
-                aria-label="close" 
-                onClick={handleClick}
-            >
-                <img 
-                    src={closeButton} 
-                    style={{width: "13px", height: "10px"}}
-                    alt=""
-                />
-            </button>
-        </div>
-    )
-}
+  const handleClick = () => {
+    console.log("Close button has been clicked");
+  };
+
+  return (
+    <div className="notification-items">
+      <button
+        style={{ float: "right" }}
+        aria-label="close"
+        onClick={handleClick}
+      >
+        <img
+          src={closeButton}
+          style={{ width: "13px", height: "10px" }}
+          alt=""
+        />
+      </button>
+
+      <p>Here is the list of notifications</p>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
+      </ul>
+    </div>
+  );
+};
 
 export default Notifications;
