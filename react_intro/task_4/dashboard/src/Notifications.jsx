8a714c75deeb121c89702848b0a337bb1c47c9a3
12785@holbertonstudents.com <you@example.com>
2026-03-12 17:34:27 +0100

    Initial commit

---
+++ react_intro/task_4/dashboard/src/Notifications.jsx
@@ -0,0 +1,25 @@
+import './Notifications.css';
+import closeButton from './assets/close-button.png'
+import { getLatestNotification } from './utils';
+
+const Notifications = () =>{
+
+    const handleClick = () =>{
+        console.log("Close button has been clicked");
+    }
+    return(
+        <div className="notification-items">
+            <p>Here is the list of notifications</p>
+            <ul>
+                <li data-priority="default">New course available</li>
+                <li data-priority="urgent">New resume available</li>
+                <li dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+            </ul>
+            <button style={{float:'right'}} aria-label="Close" onClick={handleClick}>
+                <img src={closeButton} style={{width: "13px", height: "10px"}}/>
+            </button>
+        </div>
+    )
+}
+
+export default Notifications;
