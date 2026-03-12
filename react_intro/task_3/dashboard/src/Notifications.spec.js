5cd03d30ef8e50e7508e509da90b6d1453880733
12785@holbertonstudents.com <you@example.com>
2026-03-12 14:13:57 +0100

    Initial commit

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -9,7 +9,7 @@ test("renders notifications title", () => {
 
 test("renders close button", () => {
     render(<Notifications />);
-    const button = screen.getByRole("button", { name: /close/i });
+    const button = screen.getByLabelText(/close/i);
     expect(button).toBeInTheDocument();
 });
 
@@ -22,7 +22,7 @@ test("renders 3 list items", () => {
 test("clicking close button logs to console", () => {
     const consoleSpy = jest.spyOn(console, "log");
     render(<Notifications />);
-    const button = screen.getByRole("button", { name: /close/i });
+    const button = screen.getByLabelText(/close/i);
     fireEvent.click(button);
     expect(consoleSpy).toHaveBeenCalledWith("Close button has been clicked");
     consoleSpy.mockRestore();
