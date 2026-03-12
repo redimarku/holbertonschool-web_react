67113159e38d5d3bd639cb7b5c572152019232f4
12785@holbertonstudents.com <you@example.com>
2026-03-12 13:06:57 +0100

    task_2

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -13,15 +13,12 @@ const App = () => {
         <img src={image3} alt='holberton logo' style={{ width: '300px', height: '350px' }} />
         <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-     
       <div className='App-body'>
-        <p> Login to access the full dashboard</p>
-        <label htmlFor="email">email</label>
-        <input type="email" id="email"></input>
-
-        <label htmlFor="password">password</label>
-        <input type="password" id="password"></input>
-
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
         <button>Ok</button>
       </div>
       <div className='App-footer'>
