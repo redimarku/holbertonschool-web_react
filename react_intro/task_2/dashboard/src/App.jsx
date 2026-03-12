eda44ebf04c2fddf180b6b6ec4f6650c054bb111
12785@holbertonstudents.com <you@example.com>
2026-03-12 12:53:05 +0100

    task_2

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+import image3 from './assets/holberton-logo.jpg';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Notifications />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="App-header">
+        <img src={image3} alt='holberton logo' style={{ width: '300px', height: '350px' }} />
+        <h1 style={{ color: '#e1003c' }}>School dashboard</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email'></input>
+        <br />
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password'></input>
+        <br />
+        <button type='submit'>OK</button> 
+      </div>
+      <div className='App-footer'>
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      </div>
     </>
   )
 }
---
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -0,0 +1,11 @@
+import './Notifications.css';
+
+const Notifications = () =>{
+    return(
+        <div className="notification-items">
+            <p>Here is the list of notifications</p>
+        </div>
+    )
+}
+
+export default Notifications;
