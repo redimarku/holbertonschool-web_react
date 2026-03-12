0c9e65f8a691fed0a27a783648024ed1f6e72892
12785@holbertonstudents.com <you@example.com>
2026-03-12 13:55:45 +0100

    Initial commit

--- react_intro/task_3/dashboard/src/App.spec.js
+++ react_intro/task_3/dashboard/src/App.spec.js
@@ -10,13 +10,13 @@ test("renders App form correctly", () => {
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
 
-
+ 
     const emailLabel = screen.getByText(/email/i);
     const passwordLabel = screen.getByText(/password/i);
     expect(emailLabel).toBeInTheDocument();
     expect(passwordLabel).toBeInTheDocument();
 
-    
+   
     const button = screen.getByRole("button", { name: /ok/i });
     expect(button).toBeInTheDocument();
 });
