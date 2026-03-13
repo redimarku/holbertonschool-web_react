bae7aaf504260033c89678832fcaa6c28742c123
12785@holbertonstudents.com <you@example.com>
2026-03-13 23:24:24 +0100

    Initial commit

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -2,18 +2,19 @@ import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import { Fragment } from 'react/jsx-runtime';
 
 
 function App() {
 
   return (
-    <>
+    <Fragment>
    
       <Notifications />
       <Header />
       <Login />
       <Footer />
-    </>
+    </Fragment>
   )
 }
 
