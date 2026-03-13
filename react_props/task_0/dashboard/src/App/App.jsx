2ae877e2b715fa68d8c0e0af24408f219ae6924f
12785@holbertonstudents.com <you@example.com>
2026-03-14 00:00:12 +0100

    Initial commit

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,4 +1,5 @@
-import Notifications, { Fragment } from '../Notifications/Notifications';
+import { Fragment } from 'react';
+import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
