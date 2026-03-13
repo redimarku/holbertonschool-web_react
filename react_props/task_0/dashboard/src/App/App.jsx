34c2535dba3a802c660cfd5eb07b396d0602eab4
12785@holbertonstudents.com <you@example.com>
2026-03-13 23:57:20 +0100

    Initial commit

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,8 +1,7 @@
-import Notifications from '../Notifications/Notifications';
+import Notifications, { Fragment } from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
-import { Fragment } from 'react/jsx-runtime';
 
 
 function App() {
--- react_props/task_0/dashboard/src/Login/Login.jsx
+++ react_props/task_0/dashboard/src/Login/Login.jsx
@@ -4,15 +4,13 @@ const Login = () =>{
     return(
         <div className='App-body'>
         <p> Login to access the full dashboard</p>
-        <form>
         <label htmlFor="email">email</label>
-        <input type="email" id="email"  autoComplete="username"/>
+        <input type="email" id="email" />
 
         <label htmlFor="password">password</label>
-        <input type="password" id="password" autoComplete="current-password"/>
+        <input type="password" id="password" />
 
         <button type="submit">OK</button>
-        </form>
       </div>
     )
 }
