21fdabb89ef42b41854016463f1b41f2ecff2022
12785@holbertonstudents.com <you@example.com>
2026-03-14 00:51:12 +0100

    Initial commit

--- react_props/task_1/dashboard/src/Login/Login.jsx
+++ react_props/task_1/dashboard/src/Login/Login.jsx
@@ -1,18 +1,20 @@
+import React from 'react';
 import './Login.css';
 
-const Login = () =>{
-    return(
-        <div className='App-body'>
-        <p> Login to access the full dashboard</p>
-        <label htmlFor="email">email</label>
-        <input type="email" id="email" />
+const Login = () => {
+  return (
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
 
-        <label htmlFor="password">password</label>
-        <input type="password" id="password" />
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" autoComplete="username" />
 
-        <button type="submit">OK</button>
-      </div>
-    )
-}
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" autoComplete="current-password" />
+
+      <button type="submit">OK</button>
+    </div>
+  );
+};
 
 export default Login;
