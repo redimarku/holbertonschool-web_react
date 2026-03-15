4ba70ad0521ab3972d21ec5bd897a49b4d4adb60
12785@holbertonstudents.com <you@example.com>
2026-03-16 00:43:57 +0100

    Initial commit

---
+++ react_props/task_5/dashboard/src/App/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import React from 'react';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+
+function App({ isLoggedIn = false }) {
+  const notificationsList = [
+    { id: 1, type: 'default', value: 'New course available' },
+    { id: 2, type: 'urgent', value: 'New resume available' },
+    { id: 3, type: 'urgent', html: { __html: 'Urgent requirement - complete by EOD' } },
+  ];
+
+  const coursesList = [
+    { id: 1, name: 'ES6', credit: 60 },
+    { id: 2, name: 'Webpack', credit: 20 },
+    { id: 3, name: 'React', credit: 40 },
+  ];
+
+  return (
+    <>
+      <Notifications notifications={notificationsList} />
+      <div className="App">
+        <Header />
+        {isLoggedIn ? <CourseList courses={coursesList} /> : <Login />}
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
