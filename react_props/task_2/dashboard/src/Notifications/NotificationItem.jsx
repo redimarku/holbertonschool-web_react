6c78c917ed062ac724c215e19f14e1148d498d20
12785@holbertonstudents.com <you@example.com>
2026-03-15 18:06:56 +0100

    Initial commit

--- react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function NotificationItem({ type, value, html }) {
-    const color = type === 'urgent' ? 'red' : 'blue';
+function NotificationItem({ type = 'default', value = '', html = null }) {
+  const color = type === 'urgent' ? 'red' : 'blue';
 
-    return html ? (
-        <li
-            data-notification-type={type}
-            style={{ color }}
-            dangerouslySetInnerHTML={html}
-        />
-    ) : (
-        <li data-notification-type={type} style={{ color }}>
-            {value}
-        </li>
-    );
+  return html ? (
+    <li
+      data-notification-type={type}
+      style={{ color }}
+      dangerouslySetInnerHTML={html}
+    />
+  ) : (
+    <li data-notification-type={type} style={{ color }}>
+      {value}
+    </li>
+  );
 }
 
 NotificationItem.propTypes = {
-    type: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    html: PropTypes.shape({ __html: PropTypes.string }),
-};
-
-NotificationItem.defaultProps = {
-    type: 'default',
-    value: '',
-    html: null,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  html: PropTypes.shape({ __html: PropTypes.string }),
 };
 
 export default NotificationItem;
--- react_props/task_2/dashboard/src/Notifications/Notifications.jsx
+++ react_props/task_2/dashboard/src/Notifications/Notifications.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import NotificationItem from './NotificationItem';
+import './Notifications.css';
+import closeButton from '../assets/close-button.png';
 
-function Notifications({ notifications }) {
-    return (
-        <div className="Notifications">
-            <p>Here is the list of notifications</p>
-            <ul>
-                {notifications.map((notif) => (
-                    <NotificationItem
-                        key={notif.id}
-                        type={notif.type}
-                        value={notif.value}
-                        html={notif.html}
-                    />
-                ))}
-            </ul>
-        </div>
-    );
+function Notifications({ notifications = [] }) {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+
+  return (
+    <div className="Notifications">
+      <p>Here is the list of notifications</p>
+      <ul>
+        {notifications.map((notif) => (
+          <NotificationItem
+            key={notif.id}
+            type={notif.type}
+            value={notif.value}
+            html={notif.html}
+          />
+        ))}
+      </ul>
+      <button aria-label="Close" onClick={handleClick}>
+        <img src={closeButton} alt="close" style={{ width: '13px', height: '10px' }} />
+      </button>
+    </div>
+  );
 }
 
-Notifications.defaultProps = {
-    notifications: [],
+Notifications.propTypes = {
+  notifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      type: PropTypes.string.isRequired,
+      value: PropTypes.string,
+      html: PropTypes.shape({ __html: PropTypes.string }),
+    })
+  ),
 };
 
 export default Notifications;
