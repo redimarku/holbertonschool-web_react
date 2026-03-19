448873c26f4541697078bc2dfe25dce6f697296c
12785@holbertonstudents.com <you@example.com>
2026-03-19 14:10:20 +0100

    Initial commit

--- react_component/task_1/dashboard/src/App/App.spec.js
+++ react_component/task_1/dashboard/src/App/App.spec.js
@@ -1,44 +1,49 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-import Notifications from '../Notifications/Notifications';
-import Login from '../Login/Login';
-import CourseList from '../CourseList/CourseList';
+describe('keyboard events', () => {
+  let alertMock;
 
-describe('App component', () => {
-  it('renders without crashing', () => {
+  beforeEach(() => {
+    alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    alertMock.mockRestore();
+  });
+
+  it('calls logOut when Ctrl + H is pressed', () => {
+    const logOutMock = jest.fn();
+
+    const wrapper = shallow(<App logOut={logOutMock} />);
+    
+    // Mount lifecycle manually (IMPORTANT with shallow)
+    wrapper.instance().componentDidMount();
+
+    const event = new KeyboardEvent('keydown', {
+      key: 'h',
+      ctrlKey: true,
+    });
+
+    document.dispatchEvent(event);
+
+    expect(logOutMock).toHaveBeenCalledTimes(1);
+
+    // Cleanup
+    wrapper.instance().componentWillUnmount();
+  });
+
+  it('calls alert when Ctrl + H is pressed', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper).toBeTruthy();
-  });
+    
+    wrapper.instance().componentDidMount();
 
-  it('passes notificationsList to Notifications as notifications prop', () => {
-    const wrapper = shallow(<App />);
-    const notifications = wrapper.find(Notifications);
-    expect(notifications.prop('notifications')).toHaveLength(3);
-    expect(notifications.prop('notifications')[0]).toEqual({
-      id: 1, type: 'default', value: 'New course available',
+    const event = new KeyboardEvent('keydown', {
+      key: 'h',
+      ctrlKey: true,
     });
-    expect(notifications.prop('notifications')[1]).toEqual({
-      id: 2, type: 'urgent', value: 'New resume available',
-    });
-    expect(notifications.prop('notifications')[2]).toEqual({
-      id: 3, type: 'urgent', html: { __html: 'Urgent requirement - complete by EOD' },
-    });
-  });
 
-  describe('when isLoggedIn is false', () => {
-    it('renders the Login component', () => {
-      const wrapper = shallow(<App isLoggedIn={false} />);
-      expect(wrapper.find(Login)).toHaveLength(1);
-      expect(wrapper.find(CourseList)).toHaveLength(0);
-    });
-  });
+    document.dispatchEvent(event);
 
-  describe('when isLoggedIn is true', () => {
-    it('renders the CourseList component', () => {
-      const wrapper = shallow(<App isLoggedIn={true} />);
-      expect(wrapper.find(CourseList)).toHaveLength(1);
-      expect(wrapper.find(Login)).toHaveLength(0);
-    });
+    expect(alertMock).toHaveBeenCalledWith('Logging you out');
+
+    wrapper.instance().componentWillUnmount();
   });
 });
