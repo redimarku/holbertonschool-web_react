43dd75cff25f7d92d9d5d293770a48074e079521
12785@holbertonstudents.com <you@example.com>
2026-04-09 17:08:26 +0200

    Initial commit

---
+++ react_hooks/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,139 @@
+import React, { Component, Fragment } from 'react';
+import Notifications from "../Notifications/Notifications";
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import NewContext from '../Context/context';
+import './App.css';
+
+const coursesList = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+const notificationsList = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+
+    this.state = {
+      displayDrawer: false,
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+      logOut: this.logOut,
+      notifications: notificationsList,
+      courses: coursesList,
+    };
+  }
+
+  handleKeyDown(e) {
+    if ('key' in e && e.ctrlKey && e.key === 'h') {
+      alert('Logging you out');
+      this.logOut();
+    }
+  }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  logIn(email, password) {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      },
+    });
+  }
+
+  logOut() {
+    this.setState({
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      },
+    });
+  }
+
+  markNotificationAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+    this.setState((prevState) => ({
+      notifications: prevState.notifications.filter((n) => n.id !== id),
+    }));
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  render() {
+    const { displayDrawer, user, logOut, notifications, courses } = this.state;
+
+    return (
+      <NewContext.Provider value={{ user, logOut }}>
+        <Fragment>
+          <div className="root-notifications">
+            <Notifications
+              notifications={notifications}
+              displayDrawer={displayDrawer}
+              handleDisplayDrawer={this.handleDisplayDrawer}
+              handleHideDrawer={this.handleHideDrawer}
+              markNotificationAsRead={this.markNotificationAsRead}
+            />
+          </div>
+          <Header />
+
+          {user.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList courses={courses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login
+                logIn={this.logIn}
+                email={user.email}
+                password={user.password}
+              />
+            </BodySectionWithMarginBottom>
+          )}
+
+          <BodySection title="News from the School">
+            <p>Holberton School News goes here</p>
+          </BodySection>
+
+          <Footer />
+        </Fragment>
+      </NewContext.Provider>
+    );
+  }
+}
+
+export default App;
---
+++ react_hooks/task_1/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import './Notifications.css';
+import close from '../assets/close-button.png';
+import NotificationItem from './NotificationItem';
+
+class Notifications extends Component {
+  shouldComponentUpdate(nextProps) {
+    return (
+      nextProps.notifications.length !== this.props.notifications.length ||
+      nextProps.displayDrawer !== this.props.displayDrawer
+    );
+  }
+
+  render() {
+    const {
+      notifications = [],
+      displayDrawer = false,
+      handleDisplayDrawer = () => {},
+      handleHideDrawer = () => {},
+      markNotificationAsRead,
+    } = this.props;
+
+    return (
+      <div className="Notifications">
+        <div
+          className="notification-title"
+          onClick={handleDisplayDrawer}
+          style={{ cursor: 'pointer' }}
+        >
+          Your notifications
+        </div>
+
+        {displayDrawer && (
+          <div className="notification-items">
+            <p>Here is the list of notifications</p>
+
+            {notifications.length === 0 ? (
+              <p>No new notification for now</p>
+            ) : (
+              <ul>
+                {notifications.map((notif) => (
+                  <NotificationItem
+                    key={notif.id}
+                    id={notif.id}
+                    type={notif.type}
+                    value={notif.value}
+                    html={notif.html}
+                    markAsRead={markNotificationAsRead}
+                  />
+                ))}
+              </ul>
+            )}
+
+            <button
+              aria-label="Close"
+              style={{ position: 'absolute', top: '10px', right: '10px' }}
+              onClick={handleHideDrawer}
+            >
+              <img
+                src={close}
+                alt="close"
+                style={{ width: '30px', height: '30px' }}
+              />
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Notifications;
