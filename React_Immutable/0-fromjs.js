fedc8fcffd21555119f21dbd4e767885a2f03a38
12785@holbertonstudents.com <you@example.com>
2026-04-24 14:56:32 +0200

    Initial commit

--- React_Immutable/0-fromjs.js
+++ React_Immutable/0-fromjs.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import { fromJS } from 'immutable';
 
-export function getImmutableObject(obj) {
-    return fromJS(obj);
+export default function getImmutableObject(obj) {
+  return fromJS(obj);
 }
